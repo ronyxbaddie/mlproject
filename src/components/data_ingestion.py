b9c855ac4013ba4183f9071c6ad4b12e1c52bf18
ronyxbaddie <raunakpolist5258@gmail.com>
2025-04-03 20:49:49 +0530

    dataingestion and transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,9 @@ from src.logger import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 from dataclasses import dataclass
 
 #in my data ingestion component any input that is required i will give through this class
@@ -51,4 +54,7 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
